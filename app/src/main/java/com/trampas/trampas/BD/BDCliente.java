6b7f14d09fbc27e4db8652f39e63016ba262da3b
alepeculio <alejandropeculio@gmail.com>
2019-01-09 13:43:06 -0300

    Google maps api key en release y url del servidor cambiada

--- app/src/main/java/com/trampas/trampas/BD/BDCliente.java
+++ app/src/main/java/com/trampas/trampas/BD/BDCliente.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BDCliente {
-    public static final String BASE_URL = "http://192.168.0.107/trampas/public/";
+    public static final String BASE_URL = "https://leishmaniasis.paysandu.gub.uy/";
     //public static final String BASE_URL = "http://trapps.esy.es/servidor-trampas/public/";
     private static Retrofit retrofit = null;
 
